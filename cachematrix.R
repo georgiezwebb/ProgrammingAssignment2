138cc31615d23e7a1aaa76f02da381a14b179a3b
zwebb <georigezwebb@gmail.com>
2017-01-08 12:32:40 +0000

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## makeCacheMatrix creates a new environment where a matrix object is created 
+## and its value can be set and retrieved
 makeCacheMatrix <- function(x = matrix()) {
-
+		i <- NULL
+		set <- function(y){
+			x <<- y
+			i <<- NULL
+		}
+		get <- function() x
+		setinverse <- function(solve) i <<- solve
+		getinverse <- function() i
+		list(set = set, get = get, 
+			setinverse = setinverse, 
+			getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## chacheSolve checks makeCacheMatrix that inverse has not been previously
+## calculated. If not the new inverse is calclated and added to the cached object.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+		i <- x$getinverse()
+		if(!is.null(i)){
+			message("getting cached data")
+		  ##Return previously chached matrix
+			return(i)
+		}
+		data <- x$get()
+		i <- solve(data, ...)
+		x$setinverse(i)
+		##Return new matrix
+		return(i)
 }
